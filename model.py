0f1ccdd6b0e7051edebe0c3ac30cc48425be4d51
morphisjustfun <mariojacobo2011@gmail.com>
2023-06-08 18:45:45 -0500

    aa

---
+++ model.py
@@ -0,0 +1,109 @@
+import javalang
+import numpy as np
+from javalang.tree import *
+from queue import LifoQueue, Queue
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.utils.rnn as rnn_utils
+from torch.utils.data import DataLoader, TensorDataset, Dataset
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+trainData = pd.read_csv('dist/train_df.csv')
+df_majority = trainData[trainData.bug == 0]
+df_minority = trainData[trainData.bug == 1]
+
+# Count how many samples for the majority class
+majority_count = df_majority.shape[0]
+
+# Upsample minority class
+df_minority_upsampled = df_minority.sample(majority_count, replace=True, random_state=42)
+
+# Combine majority class with upsampled minority class
+trainData_balanced = pd.concat([df_majority, df_minority_upsampled], axis=0)
+trainData_balanced = trainData_balanced.sample(frac=1, random_state=42)
+trainData_balanced = trainData_balanced.reset_index(drop=True)
+
+bfs_data = trainData_balanced.iloc[:, 0:2600]
+dfs_data = trainData_balanced.iloc[:, 2600:5200]
+labels_data = trainData_balanced['bug']
+vocabSize = bfs_data.max().max() + 1
+
+
+class Net(nn.Module):
+    def __init__(self, vocab_size):
+        super(Net, self).__init__()
+        self.bfs_embedding = nn.Embedding(vocab_size, 30)
+        self.bfs_lstm = nn.LSTM(30, 20)
+        self.dfs_embedding = nn.Embedding(vocab_size, 30)
+        self.dfs_lstm = nn.LSTM(30, 20)
+        self.fc1 = nn.Linear(40, 60)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(60, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, bfs, dfs):
+        bfs_lengths = (bfs != 0).sum(dim=1)
+        dfs_lengths = (dfs != 0).sum(dim=1)
+
+        bfs_embedded = self.bfs_embedding(bfs)
+        dfs_embedded = self.dfs_embedding(dfs)
+
+        bfs_packed = rnn_utils.pack_padded_sequence(bfs_embedded.permute(1, 0, 2), bfs_lengths.cpu(),
+                                                    enforce_sorted=False)
+        dfs_packed = rnn_utils.pack_padded_sequence(dfs_embedded.permute(1, 0, 2), dfs_lengths.cpu(),
+                                                    enforce_sorted=False)
+
+        bfs_lstm_out, _ = self.bfs_lstm(bfs_packed)
+        dfs_lstm_out, _ = self.dfs_lstm(dfs_packed)
+
+        bfs_lstm_out, _ = rnn_utils.pad_packed_sequence(bfs_lstm_out)
+        dfs_lstm_out, _ = rnn_utils.pad_packed_sequence(dfs_lstm_out)
+
+        bfs_lstm_out = bfs_lstm_out[bfs_lengths - 1, torch.arange(bfs_lengths.shape[0])]
+        dfs_lstm_out = dfs_lstm_out[dfs_lengths - 1, torch.arange(dfs_lengths.shape[0])]
+
+        # bfs_lstm_out = bfs_lstm_out[-1]
+        # dfs_lstm_out = dfs_lstm_out[-1]
+
+        merged = torch.cat((bfs_lstm_out, dfs_lstm_out), dim=1)
+        merged = self.fc1(merged)
+        merged = self.relu(merged)
+        merged = self.fc2(merged)
+        merged = self.sigmoid(merged)
+        return merged
+
+
+bfs_data, bfs_test, dfs_data, dfs_test, labels_data, labels_test = train_test_split(bfs_data, dfs_data, labels_data,
+                                                                                    test_size=0.1)
+
+train_data = TensorDataset(torch.from_numpy(bfs_data.values), torch.from_numpy(dfs_data.values),
+                           torch.from_numpy(labels_data.values))
+val_data = TensorDataset(torch.from_numpy(bfs_test.values), torch.from_numpy(dfs_test.values),
+                         torch.from_numpy(labels_test.values))
+train_loader = DataLoader(train_data, batch_size=120, shuffle=True)
+model = Net(vocabSize)
+device = torch.device("mps")
+model = model.to(device)
+optimizer = optim.Adam(model.parameters(), lr=0.01)
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.5)
+criterion = nn.BCELoss()
+for epoch in range(100):
+    model.train()
+    for i, data in enumerate(train_loader, 0):
+        bfs, dfs, labels = data
+        bfs, dfs, labels = bfs.to(device), dfs.to(device), labels.float().to(device)
+        labels = labels.view(-1, 1)
+        optimizer.zero_grad()
+        outputs = model(bfs, dfs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        val_loss = criterion(outputs, labels)
+        acc = (outputs.round() == labels).float().mean()
+    print(f"Epoch {epoch + 1}: Train Loss = {val_loss:.4f} | Train Acc = {acc:.4f}")
+    scheduler.step()
+
+# save model
+torch.save(model.state_dict(), 'dist/model.pt')
---
+++ processing.py
@@ -0,0 +1,316 @@
+# camel1.4 -> https://github.com/apache/camel
+# train with 1.4 and test with 1.6
+# WPDP -> Within Project Defect Prediction
+import javalang
+import numpy as np
+from javalang.tree import *
+from queue import LifoQueue, Queue
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.utils.rnn as rnn_utils
+from torch.utils.data import DataLoader, TensorDataset, Dataset
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+maxTokens = 2600
+
+# GET TRAIN DATA
+
+functionInstanceInvocationClasses = [MethodInvocation, SuperMethodInvocation, ClassCreator]
+declarationClasses = [PackageDeclaration, InterfaceDeclaration, ClassDeclaration, ConstructorDeclaration,
+                      MethodDeclaration, VariableDeclarator, VariableDeclaration, FormalParameter]
+controlFlowClasses = [IfStatement, ForStatement, WhileStatement, DoStatement, AssertStatement, BreakStatement,
+                      ContinueStatement, ReturnStatement, ThrowStatement, TryStatement, SynchronizedStatement,
+                      SwitchStatement, BlockStatement, CatchClauseParameter, TryResource, CatchClause,
+                      SwitchStatementCase, ForControl, EnhancedForControl]
+otherClasses = [BasicType, MemberReference, ReferenceType, SuperMemberReference, StatementExpression]
+
+
+def dfsAstNode(root):
+    q = LifoQueue()
+    q.put(root)
+    while not q.empty():
+        node = q.get()
+        if isinstance(node, Node):
+            children = node.children
+            yield node
+        else:
+            children = node
+
+        for child in children:
+            if isinstance(child, (Node, list, tuple)):
+                q.put(child)
+
+
+def bfsAstNode(root):
+    q = Queue()
+    q.put(root)
+    while not q.empty():
+        node = q.get()
+        if isinstance(node, Node):
+            children = node.children
+            yield node
+        else:
+            children = node
+
+        for child in children:
+            if isinstance(child, (Node, list, tuple)):
+                q.put(child)
+
+
+def fileExists(file) -> bool:
+    try:
+        with open(file, 'r') as _:
+            return True
+    except IOError:
+        return False
+
+
+def getDfsFilteredNodes(root):
+    dfsSequence = []
+    for node in dfsAstNode(root):
+        try:
+            if any(isinstance(node, classWanted) for classWanted in functionInstanceInvocationClasses):
+                if isinstance(node, ClassCreator):
+                    dfsSequence.append(node.type.name)
+                else:
+                    dfsSequence.append(node.member)
+            elif any(isinstance(node, classWanted) for classWanted in declarationClasses):
+                if isinstance(node, LocalVariableDeclaration) or isinstance(node, VariableDeclaration):
+                    dfsSequence.append(node.type.name)
+                else:
+                    dfsSequence.append(node.name)
+
+            elif any(isinstance(node, classWanted) for classWanted in controlFlowClasses):
+                if isinstance(node, IfStatement):
+                    dfsSequence.append('if')
+                elif isinstance(node, ForStatement):
+                    dfsSequence.append('for')
+                elif isinstance(node, WhileStatement):
+                    dfsSequence.append('while')
+                elif isinstance(node, DoStatement):
+                    dfsSequence.append('do')
+                elif isinstance(node, AssertStatement):
+                    dfsSequence.append('assert')
+                elif isinstance(node, BreakStatement):
+                    dfsSequence.append('break')
+                elif isinstance(node, ContinueStatement):
+                    dfsSequence.append('continue')
+                elif isinstance(node, ReturnStatement):
+                    dfsSequence.append('return')
+                elif isinstance(node, ThrowStatement):
+                    dfsSequence.append('throw')
+                elif isinstance(node, TryStatement):
+                    dfsSequence.append('try')
+                elif isinstance(node, SynchronizedStatement):
+                    dfsSequence.append('synchronized')
+                elif isinstance(node, SwitchStatement):
+                    dfsSequence.append('switch')
+                elif isinstance(node, BlockStatement):
+                    dfsSequence.append('block')
+                elif isinstance(node, CatchClauseParameter) or isinstance(node, TryResource):
+                    dfsSequence.append(node.name)
+                elif isinstance(node, SwitchStatementCase):
+                    dfsSequence.append(node.member)
+                elif isinstance(node, ForControl):
+                    dfsSequence.append(node.condition)
+                elif isinstance(node, EnhancedForControl):
+                    dfsSequence.append(node.var.type.name)
+
+            elif any(isinstance(node, classWanted) for classWanted in otherClasses):
+                if isinstance(node, BasicType) or isinstance(node, ReferenceType):
+                    dfsSequence.append(node.name)
+                elif isinstance(node, MemberReference) or isinstance(node, SuperMemberReference):
+                    dfsSequence.append(node.member)
+                elif isinstance(node, StatementExpression):
+                    if isinstance(node.expression, Assignment):
+                        if isinstance(node.expression.value, MemberReference):
+                            dfsSequence.append(node.expression.value.member)
+                        else:
+                            dfsSequence.append(node.expression.value.value)
+                    else:
+                        dfsSequence.append(node.expression.member)
+        except:
+            pass
+    return dfsSequence
+
+
+def getBfsFilteredNodes(root):
+    bfsSequence = []
+    for node in bfsAstNode(root):
+        try:
+            if any(isinstance(node, classWanted) for classWanted in functionInstanceInvocationClasses):
+                if isinstance(node, ClassCreator):
+                    bfsSequence.append(node.type.name)
+                else:
+                    bfsSequence.append(node.member)
+            elif any(isinstance(node, classWanted) for classWanted in declarationClasses):
+                if isinstance(node, LocalVariableDeclaration) or isinstance(node, VariableDeclaration):
+                    bfsSequence.append(node.type.name)
+                else:
+                    bfsSequence.append(node.name)
+
+            elif any(isinstance(node, classWanted) for classWanted in controlFlowClasses):
+                if isinstance(node, IfStatement):
+                    bfsSequence.append('if')
+                elif isinstance(node, ForStatement):
+                    bfsSequence.append('for')
+                elif isinstance(node, WhileStatement):
+                    bfsSequence.append('while')
+                elif isinstance(node, DoStatement):
+                    bfsSequence.append('do')
+                elif isinstance(node, AssertStatement):
+                    bfsSequence.append('assert')
+                elif isinstance(node, BreakStatement):
+                    bfsSequence.append('break')
+                elif isinstance(node, ContinueStatement):
+                    bfsSequence.append('continue')
+                elif isinstance(node, ReturnStatement):
+                    bfsSequence.append('return')
+                elif isinstance(node, ThrowStatement):
+                    bfsSequence.append('throw')
+                elif isinstance(node, TryStatement):
+                    bfsSequence.append('try')
+                elif isinstance(node, SynchronizedStatement):
+                    bfsSequence.append('synchronized')
+                elif isinstance(node, SwitchStatement):
+                    bfsSequence.append('switch')
+                elif isinstance(node, BlockStatement):
+                    bfsSequence.append('block')
+                elif isinstance(node, CatchClauseParameter) or isinstance(node, TryResource):
+                    bfsSequence.append(node.name)
+                elif isinstance(node, SwitchStatementCase):
+                    bfsSequence.append(node.member)
+                elif isinstance(node, ForControl):
+                    bfsSequence.append(node.condition)
+                elif isinstance(node, EnhancedForControl):
+                    bfsSequence.append(node.var.type.name)
+
+            elif any(isinstance(node, classWanted) for classWanted in otherClasses):
+                if isinstance(node, BasicType) or isinstance(node, ReferenceType):
+                    bfsSequence.append(node.name)
+                elif isinstance(node, MemberReference) or isinstance(node, SuperMemberReference):
+                    bfsSequence.append(node.member)
+                elif isinstance(node, StatementExpression):
+                    if isinstance(node.expression, Assignment):
+                        if isinstance(node.expression.value, MemberReference):
+                            bfsSequence.append(node.expression.value.member)
+                        else:
+                            bfsSequence.append(node.expression.value.value)
+                    else:
+                        bfsSequence.append(node.expression.member)
+        except:
+            pass
+    return bfsSequence
+
+
+componentsList = ['camel-amqp', 'camel-atom', 'camel-bam', 'camel-csv', 'camel-cxf', 'camel-flatpack',
+                  'camel-ftp',
+                  'camel-groovy', 'camel-hamcrest', 'camel-http', 'camel-ibatis', 'camel-irc',
+                  'camel-jaxb',
+                  'camel-jcr', 'camel-jdbc', 'camel-jetty', 'camel-jhc', 'camel-jing', 'camel-jms',
+                  'camel-josql',
+                  'camel-jpa', 'camel-juel', 'camel-jxpath', 'camel-mail', 'camel-mina', 'camel-msv',
+                  'camel-ognl',
+                  'camel-osgi', 'camel-quartz', 'camel-rmi', 'camel-ruby', 'camel-saxon',
+                  'camel-script',
+                  'camel-spring', 'camel-spring-integration', 'camel-sql', 'camel-stream',
+                  'camel-stringtemplate', 'camel-supercsv', 'camel-swing', 'camel-testng', 'camel-uface',
+                  'camel-velocity', 'camel-xmlbeans', 'camel-xmpp', 'camel-xstream']
+
+
+def getCammel(camel_version):
+    trainData = pd.read_csv(f'datasets/camel{camel_version}/labels.csv')
+    trainData = trainData.loc[:, ['name', 'bug']]
+    trainData['bug'] = trainData['bug'].apply(lambda x: 1 if x > 0 else 0)
+    trainDataNames = trainData.loc[:, ['name']]
+
+    trainDataNamesModified = []
+    for file in trainDataNames['name']:
+        name = file.replace('.', '/') + '.java'
+        exists = False
+        nameToTest = f'datasets/camel{camel_version}/source/camel-core/src/main/java/' + name
+        if fileExists(nameToTest):
+            trainDataNamesModified.append(
+                nameToTest)
+            continue
+        for component in componentsList:
+            nameToTest = f'datasets/camel{camel_version}/source/components/' + component + '/src/main/java/' + name
+            if fileExists(nameToTest):
+                trainDataNamesModified.append(nameToTest)
+                exists = True
+                break
+        if not exists:
+            trainData = trainData[trainData.name != file]
+
+    trainData['name'] = trainDataNamesModified
+    trainData.reset_index(drop=True, inplace=True)
+
+    # max tokens would be 7000
+    trainData = pd.concat([trainData, pd.DataFrame(columns=[str(i) for i in range(maxTokens * 2)])])
+
+    totalUniqueTokens = set()
+    totalBfsSequences = []
+    totalDfsSequences = []
+
+    for i in range(len(trainData)):
+        name = trainData['name'][i]
+        rootParsed = javalang.parse.parse(open(name).read())
+        bfsSequence = getBfsFilteredNodes(rootParsed)
+        dfsSequence = getDfsFilteredNodes(rootParsed)
+        bfsSequenceFiltered = list(filter(lambda x: isinstance(x, str), bfsSequence))
+        dfsSequenceFiltered = list(filter(lambda x: isinstance(x, str), dfsSequence))
+        totalBfsSequences.append(bfsSequenceFiltered)
+        totalDfsSequences.append(dfsSequenceFiltered)
+        totalUniqueTokens.update(bfsSequenceFiltered)
+        print(len(bfsSequenceFiltered))
+
+    # token2int = {token: ii for ii, token in enumerate(totalUniqueTokens, 1)}
+    # for i in range(len(trainData)):
+    #     bfsEncoded = [token2int[token] for token in totalBfsSequences[i]]
+    #     dfsEncoded = [token2int[token] for token in totalDfsSequences[i]]
+    #     print(len(bfsEncoded))
+    #     for j in range(len(bfsEncoded)):
+    #         trainData.at[i, str(j)] = bfsEncoded[j]
+    #     for j in range(len(dfsEncoded)):
+    #         trainData.at[i, str(j + maxTokens)] = dfsEncoded[j]
+
+    trainData.drop(['name'], axis=1, inplace=True)
+    trainData.fillna(0, inplace=True)
+    trainData['bug'] = trainData['bug'].astype(int)
+    return trainData, totalUniqueTokens, totalBfsSequences, totalDfsSequences
+
+
+def merge():
+    versions = ['1.0', '1.2', '1.4', '1.6']
+    dfs = []
+    totalUniqueTokens = set()
+    totalBfsSequences = []
+    totalDfsSequences = []
+    for version in versions:
+        df, uniqueTokens, bfsSequences, dfsSequences = getCammel(version)
+        dfs.append(df)
+        totalUniqueTokens.update(uniqueTokens)
+        totalBfsSequences.append(bfsSequences)
+        totalDfsSequences.append(dfsSequences)
+    word2int = {token: ii for ii, token in enumerate(totalUniqueTokens, 1)}
+
+    for i in range(len(dfs)):
+        for j in range(len(dfs[i])):
+            bfsEncoded = [word2int[token] for token in totalBfsSequences[i][j]]
+            dfsEncoded = [word2int[token] for token in totalDfsSequences[i][j]]
+            for k in range(len(bfsEncoded)):
+                dfs[i].at[j, str(k)] = bfsEncoded[k]
+            for k in range(len(dfsEncoded)):
+                dfs[i].at[j, str(k + maxTokens)] = dfsEncoded[k]
+
+    df = pd.concat(dfs)
+    df = df.sample(frac=1).reset_index(drop=True)
+    # move bug column to the end
+    df = df[[c for c in df if c not in ['bug']] + ['bug']]
+    df.to_csv('dist/train_df.csv', index=False)
+
+
+if __name__ == '__main__':
+    merge()
